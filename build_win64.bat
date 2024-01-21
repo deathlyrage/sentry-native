80b4164df806656d0f44be83230cb400ab28a534
deathlyrage <matthew@alderongames.com>
2024-01-21 07:53:52 -0500

    Add build_win64.bat

---
+++ build_win64.bat
@@ -0,0 +1,14 @@
+@echo off
+
+rmdir /Q /S "build"
+rmdir /Q /S "install"
+
+cmake --version
+
+SET BUILD_TYPE="RelWithDebInfo"
+
+cmake -B build -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DSENTRY_BACKEND=crashpad -DCMAKE_GENERATOR_PLATFORM=x64 -DBUILD_SHARED_LIBS=ON -DCMAKE_SYSTEM_VERSION=6.3
+cmake --build build --parallel --config %BUILD_TYPE%
+cmake --install build --prefix install --config %BUILD_TYPE%
+
+"C:\\Program Files\\7-Zip\\7z.exe" a Sentry-Native-Win64 "./install/*"
