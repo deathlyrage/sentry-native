e6476341280f148028e6e74aed4cbf44e57b4e0a
Alderon Matt <matthew@alderongames.com>
2024-05-21 04:12:29 -0400

    add build scripts for xb1 and xsx

---
+++ build_xsx.bat
@@ -0,0 +1,20 @@
+@echo on
+
+set SRC_DIR=%~dp0
+
+call "C:\Program Files (x86)\Microsoft GDK\Command Prompts\GamingXboxVars.cmd" GamingXboxScarlettVS2022 240301
+
+cd /D %SRC_DIR%
+
+rmdir /Q /S "build"
+rmdir /Q /S "install"
+
+cmake --version
+
+SET BUILD_TYPE="RelWithDebInfo"
+
+cmake -B build -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DSENTRY_BACKEND=none -DCMAKE_GENERATOR_PLATFORM=x64 -DBUILD_SHARED_LIBS=ON -DCMAKE_SYSTEM_VERSION=6.3
+cmake --build build --parallel --config %BUILD_TYPE%
+cmake --install build --prefix install --config %BUILD_TYPE%
+
+REM "C:\\Program Files\\7-Zip\\7z.exe" a Sentry-Native-XSX "./install/*"
